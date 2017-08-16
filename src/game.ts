984ac2d1c1d5cfc6e365c628123cb10f39521606
Tom <tjpalmer@tjpalmer.com>
2017-08-16 06:14:59 -0700

    Start gl

--- src/game.ts
+++ src/game.ts
@@ -4,9 +4,22 @@ export class Game {
     this.canvas = document.getElementsByTagName('canvas')[0];
     window.addEventListener('resize', this.resize);
     this.resize();
+    let gl = this.gl = this.canvas.getContext('webgl')!;
+    let program = gl.createProgram()!;
+    gl.attachShader(program, this.loadShader(vertex, gl.VERTEX_SHADER));
   }
 
   canvas: HTMLCanvasElement;
+
+  gl: WebGLRenderingContext;
+
+  loadShader(source: string, type: number) {
+    let {gl} = this;
+    let shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    return shader!;
+  }
 
   resize = () => {
     let {canvas} = this;
@@ -15,3 +28,10 @@ export class Game {
   }
 
 }
+
+let vertex = `
+  attribute vec3 position;
+  void main(void) {
+    gl_Position = vec4(position, 1.0);
+  }
+`;
